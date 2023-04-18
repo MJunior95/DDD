7f1836e70f510a4d0fcd59b8b24cea6264119046
Michel Junior <jrcassunde95@gmail.com>
2023-04-18 11:47:46 -0300

    Implementando o principio da autovalidacao

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,25 +1,34 @@
 class Customer{
 
     _id: string;
-    _name: string;
-    _address: string;
-    _active: boolean;
+    _name: string = "";
+    _address: string = "";
+    _active: boolean = true;
 
-    constructor(id:string, name:string, address:string,active:boolean) {
+    constructor(id:string, name:string) {
         this._id = id;
         this._name = name;
-        this._address = address;
-        this._active = active;
+        this.validate();
+    }
+
+    validate(){
+        if(this._name.length === 0){
+            throw new Error("Name is required");
+        }
+        if(this._id.length === 0){
+            throw new Error("Id is required");
+        }
     }
 
     changeName(name:string){
         this._name = name;
+        this.validate();
     }
 
     activate(){
         this._active = true
     }
-    
+
     desactivate(){
         this._active = false
     }
