cb76a2f4947d3bd3f52beb147b56e777d47e6633
Michel Junior <jrcassunde95@gmail.com>
2023-04-20 18:10:06 -0300

    implementando novos testes

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -45,4 +45,15 @@ describe("Customer unit testes", () =>{
 
         expect(customer.isActive()).toBe(false);
      });
+
+     it("should add reward points", () => {
+         const customer = new Customer("1","c1");
+         expect(customer.rewardPoints).toBe(0)
+
+         customer.addRewardPoints(10);
+         expect(customer.rewardPoints).toBe(10);
+
+         customer.addRewardPoints(10);
+         expect(customer.rewardPoints).toBe(20);
+     });
 })
