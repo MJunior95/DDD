7f82426a70e121c9e9806c4c68110537ed876bc5
Michel Junior <jrcassunde95@gmail.com>
2023-04-20 18:06:19 -0300

    implementando novas regras nos domain services

--- src/service/order.service.spec.ts
+++ src/service/order.service.spec.ts
@@ -1,3 +1,4 @@
+import Customer from "../entity/customer";
 import Order from "../entity/order";
 import OrderItem from "../entity/order_item";
 import OrderService from "./order.service";
@@ -16,5 +17,19 @@ it("should get total of all orders", () => {
 
     expect(total).toBe(500);
 })
+
+it("should place an order", () => {
+
+    const customer = new Customer("c1", "Customer1");
+    const item1 = new OrderItem("i1", "Item1", 10, "p1", 1);
+
+    const order = OrderService.placeOrder(customer, [item1]);
+
+    expect(customer.rewardPoints).toBe(5);
+    expect(order.total()).toBe(10);
+
+
+
+});
 
 });
